6e9e29117be5d2b858a7c3dbf44caf4df8a3a8f2
冬铭 <minghao.rmh@alibaba-inc.com>
2017-04-20 16:50:07 +0800

    init

---
+++ serve.js
@@ -0,0 +1,24 @@
+const webpack = require('webpack');
+const webpackDev = require('webpack-dev-server');
+const open = require('open');
+
+const config = require('./webpack.js')
+
+const compaire = webpack(config);
+
+const app = new webpackDev(compaire, {
+  hot : true,
+  publicPath : '/static/',
+  historyApiFallback : false,
+  stats : {
+			colors:true,
+			process : true
+	},
+  headers : {'X-Custom-Header' : 'yes'}
+})
+
+app.listen(80, function (err) {
+  console.log('listen...');
+  if(err) {console.log(err)}
+  open('http://localhost/index.html')
+})
